7ded9282f1cf1621a72432992c2d632169f92131
Tanay <tjtanay6@gmail.com>
2024-05-28 11:20:46 +0530

    added first test case to test sum when string is empty

---
+++ spec/calculator/string_calculator_spec.rb
@@ -0,0 +1,10 @@
+describe StringCalculator do
+  describe '#sum' do
+    context 'when given input is an empty string' do
+      it 'returns 0 for an empty string' do
+        calculator = StringCalculator.new
+        expect(calculator.sum('')).to eq(0)
+      end
+    end
+  end
+end
