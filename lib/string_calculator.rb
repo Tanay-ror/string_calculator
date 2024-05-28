687958f3a382adb86f357465998ff784d77fae52
Tanay <tjtanay6@gmail.com>
2024-05-28 12:01:36 +0530

    Changed sum method to handel input contains custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def sum(number)
     return 0 if number.empty?
     return number.to_i if number.to_i.to_s == number
-    number.split(",").map(&:to_i).sum
+    number.split(/,|\n|;/).map(&:to_i).sum
   end
 end
